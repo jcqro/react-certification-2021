fe840f1dc8e4834376dda66b063a725d15a50b4e
Julio Cesar Fernandez <jc.ferher@gmail.com>
2021-08-03 19:50:07 -0500

    minichallenge3 env

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const KEY = process.env.REACT_APP_YOU_TUBE_API_KEY;
-const KEY = 'AIzaSyCWN8A7FFegTnOTGfqnpIk8LvE5q18_0Lw';
+const KEY = process.env.REACT_APP_YOU_TUBE_API_KEY;
+// const KEY = '';
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3/',
   params: {
